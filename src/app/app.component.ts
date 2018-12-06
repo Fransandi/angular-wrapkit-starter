e0a63d648648247a17b318c2405568b40af49a62
Fran <fransandi@gmail.com>
2018-12-05 22:28:12 -0800

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'angular-wrapkit-starter';
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    this.SetAnchorEventHandler();
+  }
+
+  SetAnchorEventHandler() {
+    // wrapkit initialization (moved from index.html)
+    $('a').on('click', function (event) {
+      const $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 90
+      }, 1000);
+      event.preventDefault();
+      // code
+    });
+  }
 }
